cdb99ca2a65b85a2017f8e92a158abfbf47e82b5
Jibon Ahmed <jayed.swe@gmail.com>
2021-03-15 01:09:41 +0600

    added if & else statement

---
+++ 10-if-else-statement/if-else-statement .js
@@ -0,0 +1,19 @@
+
+a = 10
+if (a == 10) {
+    console.log(a);
+}
+else {
+    console.log("No error")
+}
+
+// Using conditional operator
+
+a = 10
+b = 20
+if (a == 10 && b == 30) {
+    console.log("True")
+}
+else {
+    console.log("False")
+}
