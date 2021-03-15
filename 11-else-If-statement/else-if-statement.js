b550082298b61bd20b0ce0947adb1704575aacae
Jibon Ahmed <jayed.swe@gmail.com>
2021-03-15 21:44:13 +0600

    added else if statement

---
+++ 11-else-If-statement/else-if-statement.js
@@ -0,0 +1,11 @@
+
+var i = 10;
+if (i == 5) {
+    console.log("Number is not equal", i)
+}
+else if (i == 10) {
+    console.log("Number is equal", i)
+}
+else {
+    console.log("Number is not equal", i)
+}
