6e46b48b39fedebc9bc010fcda63e999c392ec79
Jibon Ahmed <jayed.swe@gmail.com>
2021-03-15 23:11:32 +0600

    added for loop

---
+++ 12-for-loops/for-loop.js
@@ -0,0 +1,22 @@
+
+// Example
+for (i = 0; i < 5; i++) {
+    console.log(i)
+    console.log('\n');
+}
+
+// Print even
+for (i = 0; i <= 10; i++) {
+    if (i % 2 == 0) {
+        console.log("Even number", i)
+        console.log('\n');
+    }
+}
+
+
+// Print odd number
+for (i = 0; i <= 10; i++) {
+    if (i % 2 == 1) {
+        console.log("odd number", i)
+    }
+}
