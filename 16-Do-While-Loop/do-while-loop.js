921839179e88f31d4c93ebaf755b827a847ce34a
Jibon <jayed.swe@gmail.com>
2021-03-31 21:11:43 +0600

    added while loop

---
+++ 16-Do-While-Loop/do-while-loop.js
@@ -0,0 +1,24 @@
+
+// Do while loop
+var i = 1;
+do {
+
+    console.log(i);
+    i++;
+    console.log("\n")
+}
+
+while (i < 5);
+
+// do while loop using if
+
+var n = 0;
+do {
+    if (n == 4) {
+        break;
+    }
+    console.log(n);
+    n++;
+
+}
+while (n <= 5)
