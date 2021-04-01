67f653b442c8b626de68d5ae8c4c556725aed40f
Jibon <jayed.swe@gmail.com>
2021-04-01 22:29:45 +0600

    added break & continue js

---
+++ 18-Break-and-Continue/break-and-continue.js
@@ -0,0 +1,20 @@
+
+// Break
+
+var i = 0;
+for (i; i <= 5; i++) {
+    if (i == 4) {
+        break;
+    }
+    console.log(i);
+}
+console.log('\n')
+
+// continue
+var i = 0;
+for (i; i <= 5; i++) {
+    if (i == 4) {
+        continue;
+    }
+    console.log(i);
+}
