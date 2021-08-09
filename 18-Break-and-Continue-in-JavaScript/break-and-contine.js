5d1c791468d5e52672471d978d16b596b35b783d
Jibon <jayed.swe@gmail.com>
2021-08-10 00:08:25 +0600

    added brean and continte in javascripts

---
+++ 18-Break-and-Continue-in-JavaScript/break-and-contine.js
@@ -0,0 +1,21 @@
+
+// Break and continue 
+
+// Example of break
+for (i= 1; i<=10; i++){
+    if (i==5){
+        break;
+    }
+    console.log(i);
+}
+
+// Line Break
+console.log("\n");
+
+// Example of continue
+for (i= 1; i<=10; i++){
+    if (i==5){
+        continue;
+    }
+    console.log(i);
+}
